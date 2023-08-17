944dd1c3ef00c737bce8ea724c84dd8d6d53236f
RobertTLange <robertlange0@gmail.com>
2023-08-17 11:44:08 +0200

    Import fix

--- neuroevobench/blines/bayes_opt_jax.py
+++ neuroevobench/blines/bayes_opt_jax.py
@@ -7,9 +7,9 @@ import jax.numpy as jnp
 from flax import struct
 from evosax.core import ParameterReshaper, FitnessShaper
 from evosax.utils import get_best_fitness_member
-from neuroevobench.blines.bo_utils.bo_acq import ACQ, select_acq
-from neuroevobench.blines.bo_utils.bo_gp import DataTypes, GParameters, train
-from neuroevobench.blines.bo_utils.bo_opt import suggest_next
+from .bo_utils.bo_acq import ACQ, select_acq
+from .bo_utils.bo_gp import DataTypes, GParameters, train
+from .bo_utils.bo_opt import suggest_next
 
 Array = Any
 dtypes = DataTypes(integers=[])
